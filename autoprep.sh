f880d96caceec305bc6e11ac4cf2df7c3a3e6011
root <root@rhel7.lan>
2017-01-17 13:01:18 +0100

    modified:   autoprep.sh

--- autoprep.sh
+++ autoprep.sh
@@ -1,13 +1,35 @@
 #!/bin/bash
 
+
+debian8_prep() {
+echo " Upgrade the system"
+sudo apt-get update && sudo apt-get -y upgrade
+echo "Virtualization host installation"
+sudo apt-get -y install qemu-kvm libvirt-bin virtinst virt-viewer libguestfs-tools
+}
+
+centos7_prep() { 
 echo " Upgrade the system"
 sudo yum -y upgrade
 echo "Virtualization host installation"
 sudo yum -y group install "Virtualization Host"
-echo "Virtualisation tools installation"
 sudo yum -y install virt-manager libvirt virt-install qemu-kvm xauth dejavu-lgc-sans-fonts virt-top libguestfs-tools virt-viewer
+}
+
+services_activation() {
 echo "Activate all those services"
 sudo systemctl stop firewalld
 sudo systemctl restart libvirtd
 sudo systemctl restart httpd
 sudo virt-host-validate
+}
+
+if [ -f /etc/debian_version ]; then
+debian8_prep
+sudo virsh net-start default
+sudo virsh net-autostart default
+services_activation
+elif [ -f /etc/redhat-release ]; then
+centos7_prep
+services_activation
+fi
