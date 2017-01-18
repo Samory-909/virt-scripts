af25ab69a2cba284ebbcfb4502a346d4251e10d2
root <root@rhel7.lan>
2017-01-18 12:07:25 +0100

    modified:   auto-install-tui.sh

--- auto-install-tui.sh
+++ auto-install-tui.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
+# Demo to add interactive text dialog boxes to the auto-install.sh script
 
-vmname=$(whiptail --title "Virt-Scripts Nom de la VM" --inputbox "Quel est le nom de la VM ?" 10 60 vm1  3>&1 1>&2 2>&3)
-distro=$(whiptail --title "Menu Box" --radiolist "Choisissez votre distriubtion linux" 10 60 3 \
+# Input box to get the domain name
+vmname=$(whiptail --title "Virt-Scripts - Nom de la VM" --inputbox "Quel est le nom de la VM ?" 10 60 vm1  3>&1 1>&2 2>&3)
+exitstatus=$?
+if [ $exitstatus = 0 ]; then
+# Radiolist with the distros
+distro=$(whiptail --title "Virt-Scripts - Distribution" --radiolist "Choisissez votre distriubtion linux" 10 60 3 \
 "ubuntu" "Ubuntu 16.04 LTS" ON \
 "debian" "Debian 8 (Jessie)" OFF \
 "centos" "Centos 7" OFF  3>&1 1>&2 2>&3)
@@ -11,4 +16,6 @@ if [ $exitstatus = 0 ]; then
 else
     echo "vous avez annulé"
 fi
-
+else
+    echo "vous avez annulé"
+fi
