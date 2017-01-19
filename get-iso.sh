502d649d754e1b789401fd2cdecdce24f52fe467
root <root@7dd18f2b56a5>
2017-01-19 12:49:18 +0000

    modified:   get-iso.sh

--- get-iso.sh
+++ get-iso.sh
@@ -3,7 +3,7 @@
 os=$1
 
 location=/var/lib/iso
-url_ubuntu_iso=http://releases.ubuntu.com/16.04/ubuntu-16.04.1-server-amd64.iso
+url_ubuntu_iso=http://cdimage.debian.org/debian-cd/current/amd64/iso-cd/debian-8.7.1-amd64-netinst.iso
 url_debian_iso=http://cdimage.debian.org/debian-cd/8.6.0/amd64/iso-cd/debian-8.6.0-amd64-netinst.iso
 url_centos_iso=http://ftp.belnet.be/ftp.centos.org/7/isos/x86_64/CentOS-7-x86_64-DVD-1611.iso
 
