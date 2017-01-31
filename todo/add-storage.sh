664f48a669bc43d075c73d9a9f494f8c9c84ba98
root <root@debian8-host>
2017-01-31 19:16:33 +0100

    renommé :         add-storage.sh -> todo/add-storage.sh

--- todo/add-storage.sh
+++ todo/add-storage.sh
@@ -7,12 +7,12 @@ device=$3
 #device=vdb
 
 # Create 4G Spare Disk with dd
-#dd if=/dev/zero of=$disk  bs=1M seek=4096 count=0
+dd if=/dev/zero of=$disk  bs=1M seek=4096 count=0
 # Or create a qcow2 disk with qemu-img
-qemu-img create -f qcow2 -o preallocation=metadata $disk 4G
+#qemu-img create -f qcow2 -o preallocation=metadata $disk 4G
 # Or create a qcow2 disk with libvirt
 #virsh vol-create-as default $2.img --capacity 4G --format qcow2 --prealloc-metadata
 # Attach the disk on live guest with persistence
 virsh attach-disk $guest $disk $device --cache none --live --persistent
 # Detach the disk
-virsh detach-disk $guest $disk --persistent --live
+#virsh detach-disk $guest $disk --persistent --live
