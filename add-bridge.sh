0bc05876887ea739df8db05ca1d621d055518fab
root <root@debian8-host>
2017-02-25 00:19:49 +0100

    modifié :         add-bridge.sh

--- add-bridge.sh
+++ add-bridge.sh
@@ -1,10 +1,67 @@
 #!/bin/bash
-# Create an isolated bridge
-x=$1
-type=$2
-bridge="virbr$x"
-name=net$x
+# Create an isolated or a nat/ipv6 bridge <name> <interface> <type>
+name=$1
+bridge=$2
+# 'isolated' or 'nat'
+type=$3
 path=/tmp
+net_id1="$(shuf -i 0-255 -n 1)"
+net_id2="$(shuf -i 0-255 -n 1)"
+# random /24 in 10.0.0.0/8 range
+ip4="10.${net_id1}.${net_id2}."
+ip6="fd00:${net_id1}:${net_id2}::"
+# Fix your own range
+#ip4="192.168.1."
+#ip6="fd00:1::"
+
+check_name () {
+if [ -e /run/libvirt/network/$name.xml ] ; then
+echo "Used name : exit."
+echo "change the bridge name or do 'virsh net-destroy $name'"
+exit
+fi
+#echo OK
+}
+
+check_interface () {
+intlist=$(echo $(ls /sys/class/net))
+for x in $intlist ; do
+if [ $x = $bridge ] ; then
+echo "Used interface : exit"
+echo "change the bridge interface $bridge"
+exit
+fi
+done
+#echo OK
+}
+
+validate_ip_range () {
+
+check_ip4 () {
+iplist=$(echo $(ip -4 route | awk '{ print $1; }' | sed 's/\/.*$//'))
+for ipint in $iplist ; do
+if [ $ipint = ${ip4}0 ] ; then
+echo Alea Error
+exit
+fi
+done
+}
+
+check_ip6 () {
+iplist=$(echo $(ip -6 route | awk '{ print $1; }' | sed 's/\/.*$//'))
+for ipint in $iplist ; do
+if [ $ipint = $ip6 ] ; then
+echo Alea Error
+exit
+fi
+done
+}
+
+check_ip4
+check_ip6
+
+}
+
 
 isolated () {
 cat << EOF > $path/$name.xml
@@ -26,27 +83,54 @@ cat << EOF > $path/$name.xml
   </forward>
   <bridge name='$bridge' stp='on' delay='0'/>
   <domain name='$name'/>
-  <ip address='192.168.10${x}.1' netmask='255.255.255.0'>
+  <ip address='${ip4}1' netmask='255.255.255.0'>
     <dhcp>
-      <range start='192.168.10${x}.128' end='192.168.10${x}.150'/>
+      <range start='${ip4}128' end='${ip4}150'/>
     </dhcp>
   </ip>
-  <ip family='ipv6' address='fd00:10${x}::1' prefix='64'>
+  <ip family='ipv6' address='${ip6}1' prefix='64'>
     <dhcp>
-      <range start='fd00:10${x}::100' end='fd00:10${x}::1ff'/>
+      <range start='${ip6}100' end='${ip6}1ff'/>
     </dhcp>
   </ip>
 </network>
 EOF
 }
 
-if [ type=isolated ] ; then
-    isolated
-fi
-if [ type=nat ] ; then
-    nat_ipv6
-fi
+report_nat_ipv6 () {
+cat << EOF > ~/$name_report.txt
+Bridge Name         : $name
+Bridge Interface    : $bridge
+------------------------------------------------------------
+Bridge IPv4 address : ${ip4}1/24
+IPv4 range          : ${ip4}0 255.255.255.0
+DHCP range          : ${ip4}128 - ${ip4}150
+Bridge IPv6 address : ${ip6}1
+IPv6 range          : ${ip6}::/64
+DHCPv6 range        : ${ip6}128/24 - ${ip4}150/24
+EOF
+echo "~/$name_report.txt writed : "
+cat ~/$name_report.txt
+}
 
+check_type () {
+case $type in
+    isolated) isolated ;;
+    nat) nat_ipv6 ; report_nat_ipv6 ;;
+    *) echo "isolated or nat ? exit" ;;
+esac
+}
+
+create_bridge () {
+#cat $path/$name.xml
 virsh net-destroy $name
 virsh net-create $path/$name.xml
 #virsh net-autostart $name
+}
+
+validate_ip_range
+check_name
+check_interface
+check_type
+create_bridge
+virsh net-list
