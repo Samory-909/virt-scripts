649e16143e2606285a3ae9ccb8edae4faa81534f
goffinet <goffinet@goffinet.org>
2017-03-04 19:20:50 +0100

    modifié :         get_and_install_openwrt.sh

--- get_and_install_openwrt.sh
+++ get_and_install_openwrt.sh
@@ -1,29 +1,44 @@
 #!/bin/bash
-##Router Firewall with two interfaces
-## WAN interface is default KVM bridge virbr0
-##Create an isloted bridge named LAN on virbr3 with this xml file :
-#<network>
-#  <name>lan</name>
-#  <bridge name='virbr3' stp='on' delay='0'/>
-#</network>
-##Enable the bridge :
-#virsh net-start lan
-#virsh net-autostart lan
-#
+## OpenWRT 15.05 router Firewall with two interfaces
+# Fix variables
 name=$1
 url=https://downloads.openwrt.org/chaos_calmer/15.05/x86/kvm_guest/openwrt-15.05-x86-kvm_guest-combined-ext4.img.gz
 destination=/var/lib/libvirt/images/
-if [ $# -lt 1 ]; then
-echo "Please provide the VM name" ; exit
-else
+
+check_parameters () {
+# Check parameters
+if [ $i{#} -ne 1 ]; then
+echo "Please provide the name" ; exit
+exit
+fi
+# Check the name
+if grep -qw ${name} <<< $(virsh list --all --name)  ; then
+echo "Please provide a guest name that is not in use : exit"
+exit
+fi
+}
+
+bridges_creation () {
+# bridges creation
+./add-bridge.sh lan-$name isolated
+./add-bridge.sh internet-$name nat
+}
+
+openwrt_installation () {
+# Get and decompresse image
 wget $url$image -O $destination$name.img.gz
 gunzip $destination$name.img.gz
+# Install the guest
 virt-install --name=$name \
 --ram=128 --vcpus=1 \
 --os-type=linux \
 --disk path=$destination$name.img,bus=ide \
---network bridge=virbr3,model=virtio \
---network bridge=virbr0,model=virtio \
+--network bridge=lan-$name,model=virtio \
+--network bridge=internet-$name,model=virtio \
 --import  \
 --noautoconsole
-fi
+}
+
+check_parameters
+bridges_creation
+openwrt_installation
