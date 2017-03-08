0d6a420d9e8a4ee7fe3cafb9997038f929f54eac
goffinet <goffinet@goffinet.org>
2017-03-08 21:56:03 +0100

    modifié :         define-guest-image.sh

--- define-guest-image.sh
+++ define-guest-image.sh
@@ -81,9 +81,11 @@ fi
 
 ## Local image copy to the default storage pool ##
 cp /var/lib/libvirt/images/$image /var/lib/libvirt/images/$disk
-## Customize this image
+
+## Customize this new guest disk 
 virt-sysprep -a /var/lib/libvirt/images/$disk --hostname $name --selinux-relabel --quiet
 #virt-sysprep -a guest.img --firstboot ./yum-update.sh --firstboot ./set-desktop.sh
+#virt-sysprep --script --run-command --upload
 
 ## Import and lauch the new guest ##
 virt-install \
