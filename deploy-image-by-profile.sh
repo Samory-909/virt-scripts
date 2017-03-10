b3fc579595301b2af402b21c3b85ea4cf36a2af9
goffinet <goffinet@goffinet.org>
2017-03-10 09:55:03 +0100

    nouveau fichier : deploy-image-by-profile.sh

---
+++ deploy-image-by-profile.sh
@@ -0,0 +1,106 @@
+#!/bin/bash
+# This script define a centos7 guest by profile and network connection
+# Image name : 'centos7', ...
+imagename="centos7"
+image="centos7.qcow2"
+#image="$4.qcow2"
+# Generate an unique string
+uuid=$(uuidgen -t)
+name=$1
+# Nested (default no)
+nested=""
+#nested="--cpu host-passthrough"
+network=$2
+# Profiles : xsmall, small, medium, big (and  desktop)
+profile=$3
+parameters=$#
+
+profile_definition () {
+# VCPUs
+vcpu="1"
+# The new guest disk name
+disk="${name}-${uuid:25}.qcow2"
+# Diskbus can be 'ide', 'scsi', 'usb', 'virtio' or 'xen'
+diskbus="virtio"
+size="8"
+# Hypervisor can be 'qemu', 'kvm' or 'xen'
+hypervisor="kvm"
+# Graphics 'none' or 'vnc'
+graphics="none"
+# RAM in Mb
+memory="256"
+# Network interface and model 'virtio' or 'rtl8139' or 'e1000'
+model="virtio"
+case "$profile" in
+    xsmall) ;;
+    small) memory="512" ;;
+    medium) memory="1024" ;;
+    big) vcpu="2"
+         memory="2048" ;;
+    desktop) ;;
+    *)  echo "Please, provide a profile name : xsmall, small, medium, big, desktop"; exit;;
+esac
+}
+
+## Download the image dialog function : list, choice, sure, download
+usage_message () {
+echo "Usage : $0 <name> <network_name> <profile> <image>"
+echo "Please download one of those images in /var/lib/libvirt/images :"
+echo "centos7 is the image by default"
+for x in $imagename ; do
+echo "https://get.goffinet.org/kvm/${x}.qcow2"
+echo "Profiles available : xsmall, small, medium, big, desktop"
+done
+}
+
+check_paramters () {
+## Check parameters
+if [ "$parameters" -ne 3  ] ; then usage_message ; exit ; fi
+# check a valid image name
+#if grep -qvw "$2" <<< "$imagename" ; then usage_message ; exit ; fi
+# check the presence of the image
+if [ ! -f /var/lib/libvirt/images/${image}  ] ; then usage_message ; exit ; fi
+# Check the usage of the requested domain
+if grep -qw ${name} <<< $(virsh list --all --name)  ; then echo "Please provide an other guest name : exit" ; exit; fi
+# Check the network
+if [ ! -e /run/libvirt/network/${network}.xml ] ; then echo "$network network does not exist"
+echo "Please create a new one or choose a valid present network : " ; virsh net-list ; exit; fi
+}
+
+copy_image () {
+## Local image copy to the default storage pool ##
+cp /var/lib/libvirt/images/$image /var/lib/libvirt/images/$disk
+## Add disk resizing
+}
+
+customize_new_disk () {
+## Customize this new guest disk 
+virt-sysprep -a /var/lib/libvirt/images/$disk --hostname $name --selinux-relabel --quiet
+#virt-sysprep -a guest.img --firstboot ./yum-update.sh --firstboot ./set-desktop.sh
+#virt-sysprep --script --run-command --upload
+}
+
+import_launch () {
+## Import and lauch the new guest ##
+virt-install \
+--virt-type $hypervisor \
+--name=$name \
+--disk path=/var/lib/libvirt/images/$disk,size=$size,format=qcow2,bus=$diskbus \
+--ram=$memory \
+--vcpus=$vcpu \
+--os-variant=linux \
+--network network=$network,model=$model \
+--graphics $graphics \
+--console pty,target_type=serial \
+--import \
+--noautoconsole $nested
+}
+
+start_time="$(date -u +%s)"
+profile_definition
+check_paramters
+copy_image
+customize_new_disk
+import_launch
+end_time="$(date -u +%s)"
+echo "Time elapsed $(($end_time-$start_time)) second"
