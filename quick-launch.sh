d74a43d003bb5f33d514a61e50f6933b835b747d
goffinet <goffinet@goffinet.org>
2018-11-04 21:30:45 +0100

    Update quick-launch.sh

--- quick-launch.sh
+++ quick-launch.sh
@@ -85,7 +85,11 @@ fi
 qemu-img create -f qcow2 -b /var/lib/libvirt/images/$image /var/lib/libvirt/images/$disk
 
 ## Customize this new guest disk
+if [ $image = "ubuntu1804.qcow2" ]; then
+sleep 1
+else
 virt-sysprep -a /var/lib/libvirt/images/$disk --hostname $name --selinux-relabel  --quiet
+fi
 #virt-sysprep -a guest.img --firstboot ./yum-update.sh --firstboot ./set-desktop.sh
 #virt-sysprep --script --run-command --upload
 
