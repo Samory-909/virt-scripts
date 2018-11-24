0be7daecda19702ff074f4649d6a63415e20b42d
goffinet <goffinet@goffinet.org>
2018-11-24 22:49:11 +0100

    Update define-guest-image.sh

--- define-guest-image.sh
+++ define-guest-image.sh
@@ -57,6 +57,7 @@ os="ubuntu17.10"
 fi
 if [ $image = "centos7.qcow2" ]; then
 os="centos7.0"
+selinux="--selinux-relabel"
 fi
 
 ## Download the image dialog function : list, choice, sure, download
@@ -94,8 +95,7 @@ fi
 cp /var/lib/libvirt/images/$image /var/lib/libvirt/images/$disk
 
 ## Customize this new guest disk
-virt-sysprep -a /var/lib/libvirt/images/$disk --hostname $name --quiet
-#virt-sysprep -a /var/lib/libvirt/images/$disk --hostname $name --selinux-relabel --quiet
+virt-sysprep -a /var/lib/libvirt/images/$disk --hostname $name $selinux --quiet
 #virt-sysprep -a guest.img --firstboot ./yum-update.sh --firstboot ./set-desktop.sh
 #virt-sysprep --script --run-command --upload
 
