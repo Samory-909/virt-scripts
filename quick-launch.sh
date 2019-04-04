b51a369e17452273bf1082d098fedd0bb29cd06f
goffinet <goffinet@goffinet.org>
2019-04-04 08:07:06 +0200

    update password recovery link https://linux.goffinet.org/06-02-demarrage-du-systeme-linux/#5-password-recovery

--- quick-launch.sh
+++ quick-launch.sh
@@ -3,7 +3,7 @@
 ## First download all the qcow2 images on https://get.goffinet.org/kvm/         ##
 ## Usage : bash define-guest.sh <name> <image>                                  ##
 ## Reset root password with the procedure :                                     ##
-## https://linux.goffinet.org/07_processus_et_demarrage.html#8-password-recovery##
+## https://linux.goffinet.org/06-02-demarrage-du-systeme-linux/#5-password-recovery##
 ##################################################################################
 ## Please check all the variables
 # First parmater as name
