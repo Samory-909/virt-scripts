32727c31d97ab8379a4b83ef399f53c7735aa0a0
goffinet <goffinet@goffinet.org>
2019-05-10 10:43:02 +0200

    Update download-images.sh

--- download-images.sh
+++ download-images.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Download kvm images
+# Download kvm images builded with https://github.com/goffinet/packer-kvm from http://get.goffinet.org/kvm/
 
 #imagename="debian7 debian8 centos7 centos7.5 ubuntu1604 ubuntu1804 metasploitable kali arch"
 which curl > /dev/null || ( echo "Please install curl" && exit )
@@ -10,27 +10,45 @@ destination=/var/lib/libvirt/images/
 parameters=$#
 wd=$PWD
 
-if [ ${parameters} -ne 1 ]; then
-echo "Please provide the image name : "
-echo ${imagename}
-exit
-fi
-if [ ! -f ${destination}${image}  ] ; then
-echo "The image ${destination}${image} does not exist."
-echo "Do you want download this file ${image}"
+download_image  () {
+echo "Do you want anyway download this file ${image}"
 read -r -p "Are you sure? [y/N] " response
 case "$response" in
     [yY][eE][sS]|[yY])
         sleep1
+        curl ${url}${image} -o ${destination}${image}
+        curl ${url}${image}.sha1 -o ${destination}${image}.sha1
+        cd ${destination}
+        sha1sum -c ${image}.sha1
+        rm -rf ${image}.sha1
+        cd ${wd}
         ;;
     *)
         exit
         ;;
 esac
+}
+
+if [ ${parameters} -ne 1 ] ; then
+echo "Please provide the image name : "
+echo ${imagename}
+exit
 fi
-wget ${url}${image} -O ${destination}${image}
-wget ${url}${image}.sha1 -O ${destination}${image}.sha1
+if [ -f ${destination}${image}  ] ; then
+echo "The image ${destination}${image} already exists."
 cd ${destination}
-sha1sum -c ${image}.sha1
-rm -rf ${image}.sha1
+remote_sha1="$(curl -s ${url}${image}.sha1)"
+cd ${destination}
+local_sha1="$(sha1sum ${url}${image})"
 cd ${wd}
+if [ "${remote_sha1}" = "${local_sha1}" ] ; then
+echo "The local image is exactly the same than the remote"
+download_image
+else
+echo "The local image is not the same than the remote"
+download_image
+fi
+else
+echo "The image ${destination}${image} does not exist."
+download_image
+fi
