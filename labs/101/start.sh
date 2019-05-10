1dd63da8479c78d0af1e6ac0c452c2d244e6ba7b
goffinet <goffinet@goffinet.org>
2019-05-10 20:32:09 +0200

    update routing labs

--- labs/101/start.sh
+++ labs/101/start.sh
@@ -4,6 +4,6 @@ for x in lan wan ; do virsh net-destroy $x${name} ; virsh net-undefine $x${name}
 ./add-bridge.sh lan${name} isolated
 ./add-bridge.sh wan${name} full
 for x in r pc1- ; do virsh destroy $x${name} ; virsh undefine $x${name} --remove-all-storage ; done
-./deploy-image-by-profile.sh r${name} lan${name} small
+./deploy-image-by-profile.sh r${name} lan${name} small centos7
 ./add-nic.sh r${name} wan${name}
-./deploy-image-by-profile.sh pc1-${name} lan${name} xsmall
+./deploy-image-by-profile.sh pc1-${name} lan${name} xsmall centos7
--- labs/103/start.sh
+++ labs/103/start.sh
@@ -3,7 +3,7 @@ name="103"
 
 start_time="$(date -u +%s)"
 
-for x in lan1 lan2 lan3 lan4 wan ; do 
+for x in lan1 lan2 lan3 lan4 wan ; do
 virsh net-destroy $x${name} ; virsh net-undefine $x${name}
 ./add-bridge.sh ${x}-${name} isolated
 done
@@ -13,7 +13,7 @@ for x in r pc ; do virsh destroy $x${name} ; virsh undefine $x${name} --remove-a
 for x in 1 2 3 4 ; do
 for y in r pc ; do
 virsh destroy ${y}${x}-${name} ; virsh undefine ${y}${x}-${name} --remove-all-storage
-./deploy-image-by-profile.sh ${y}${x}-${name} lan${x}-${name} xsmall ; done
+./deploy-image-by-profile.sh ${y}${x}-${name} lan${x}-${name} xsmall centos7 ; done
 ./add-nic.sh r${x}-${name} wan-${name}
 done
 
