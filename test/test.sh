852b84e9e28d4ad170b0c54c7e31724fe071e4ef
goffinet <goffinet@goffinet.org>
2019-05-12 21:32:44 +0200

    Update test.sh

--- test/test.sh
+++ test/test.sh
@@ -3,12 +3,6 @@
 os="$1"
 version="$2"
 
-# build an image with virt-scripts
-virsh undefine ${os} --remove-all-storage || \
-./auto-install.sh ${os} ${os} && ./sparsify.sh ${os} && \
-virsh undefine ${os} && \
-mv /var/lib/libvirt/images/${os}.qcow2 /var/lib/libvirt/images/${os}${version}.qcow2
-
 # Deploy and test guests
 for x in {1..5} ; do ./define-guest-image.sh ${os}-$x ${os}${version} ; done && \
 sleep 90 && \
