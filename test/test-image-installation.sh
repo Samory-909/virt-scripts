5ddeadfa3d25e22d1ffa4a6d6f6a8b8368f0de81
goffinet <goffinet@goffinet.org>
2019-05-13 10:52:10 +0200

    Update test-image-installation.sh

--- test/test-image-installation.sh
+++ test/test-image-installation.sh
@@ -5,8 +5,9 @@ version="$2"
 
 ./auto-install.sh ${os} ${os}
 du -h /var/lib/libvirt/images/${os}.qcow2
-#./sparsify.sh ${os}
-#du -h /var/lib/libvirt/images/${os}.qcow2
-#virsh undefine ${os}
-#mv /var/lib/libvirt/images/${os}.qcow2 /var/lib/libvirt/images/${os}${version}.qcow2
-#du -h /var/lib/libvirt/images/${os}${version}.qcow2
+sleep 30
+./sparsify.sh ${os}
+du -h /var/lib/libvirt/images/${os}.qcow2
+virsh undefine ${os}
+mv /var/lib/libvirt/images/${os}.qcow2 /var/lib/libvirt/images/${os}${version}.qcow2
+du -h /var/lib/libvirt/images/${os}${version}.qcow2
